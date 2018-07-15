4f3408dd51a590d6148a47b45c4abc3b15081e54
jibojun <jbj19910123@163.com>
2018-07-15 23:14:37 +0800

    init

---
+++ seckill-service/src/main/java/com/seckill/service/CreateOrder.java
@@ -0,0 +1,9 @@
+package com.seckill.service;
+
+/**
+ * @Author: Bojun Ji
+ * @Description:
+ * @Date: 2018/7/15_11:13 PM
+ */
+public interface CreateOrder {
+}
---
+++ seckill-web/src/main/java/com/seckill/web/controller/SecKillController.java
@@ -0,0 +1,9 @@
+package com.seckill.web.controller;
+
+/**
+ * @Author: Bojun Ji
+ * @Description:
+ * @Date: 2018/7/15_11:14 PM
+ */
+public class SecKillController {
+}
