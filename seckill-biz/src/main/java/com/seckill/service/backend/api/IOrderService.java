7cd0314df7490c6dbb659241fafd3b9c63fcf044
jibojun <jbj19910123@163.com>
2018-09-13 00:59:57 +0800

    update

---
+++ seckill-biz/src/main/java/com/seckill/service/backend/api/IOrderService.java
@@ -0,0 +1,11 @@
+package com.seckill.service.backend.api;
+
+
+/**
+ * @Author: Bojun Ji
+ * @Description:
+ * @Date: 2018/8/4_2:26 AM
+ */
+public interface IOrderService {
+    boolean createOrder(String itemId, int buyNumber);
+}
--- seckill-biz/src/main/java/com/seckill/service/impl/OrderServiceImpl.java
+++ seckill-biz/src/main/java/com/seckill/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.seckill.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.seckill.entity.input.CreateOrderInput;
 import com.seckill.entity.vo.ReturnMsgVo;
 import com.seckill.service.IOrderService;
@@ -13,9 +14,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl implements IOrderService {
 
+    @Reference
+    private com.seckill.service.backend.api.IOrderService orderService;
 
     @Override
     public ReturnMsgVo createOrder(CreateOrderInput input) {
+        ReturnMsgVo
+        orderService.createOrder()
         return null;
     }
 }
