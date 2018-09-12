e98abcd8f5e5a8a9c2e13d61e91e1be7de9ba72a
jibojun <jbj19910123@163.com>
2018-09-13 01:11:41 +0800

    update

--- seckill-biz/src/main/java/com/seckill/service/impl/OrderServiceImpl.java
+++ seckill-biz/src/main/java/com/seckill/service/impl/OrderServiceImpl.java
@@ -4,7 +4,9 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.seckill.entity.input.CreateOrderInput;
 import com.seckill.entity.vo.ReturnMsgVo;
 import com.seckill.service.IOrderService;
+import com.seckill.service.enumeration.ErrorCodeEnum;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * @Author: Bojun Ji
@@ -18,9 +20,13 @@ public class OrderServiceImpl implements IOrderService {
     private com.seckill.service.backend.api.IOrderService orderService;
 
     @Override
-    public ReturnMsgVo createOrder(CreateOrderInput input) {
-        ReturnMsgVo
-        orderService.createOrder()
-        return null;
+    public ReturnMsgVo<Boolean> createOrder(CreateOrderInput input) {
+        ReturnMsgVo tmpResult = ReturnMsgVo.createFailedResult(ErrorCodeEnum.PARAM_ERROR.getMessage());
+        if (input == null || StringUtils.isEmpty(input.getItemId()) || input.getBuyNumber() == 0) {
+            return tmpResult;
+        }
+        boolean result = orderService.createOrder(input.getItemId(), input.getBuyNumber());
+        tmpResult = ReturnMsgVo.createSuccessfulResult(result);
+        return tmpResult;
     }
 }
